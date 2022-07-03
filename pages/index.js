c7ad60a17e7d29eba31708c5c28810b728c8e072
Nicolas Caro <trynx93@gmail.,com>
2022-07-03 23:23:37 +0300

    Test text on index

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,24 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import Footer from "@components/Footer";
+import Header from "@components/Header";
+import Head from "next/head";
 
 export default function Home() {
-  return (
-    <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    return (
+        <div className='container'>
+            <Head>
+                <title>Next.js Starter!</title>
+                <link rel='icon' href='/favicon.ico' />
+            </Head>
 
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
+            <main>
+                <Header title='Hola Puto :)' />
+                <p className='description'>
+                    Empeza a hacerlo lindo ^_^
+                    {/* Get started by editing <code>pages/index.js</code> */}
+                </p>
+            </main>
 
-      <Footer />
-    </div>
-  )
+            <Footer />
+        </div>
+    );
 }
