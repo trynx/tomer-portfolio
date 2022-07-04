94e2871a01055f96d13c3627ce38210f9c54460f
Nicolas Caro <trynx93@gmail.,com>
2022-07-05 00:37:05 +0300

    Add Lottie animation and change favicon

--- cypress/e2e/basic.cy.js
+++ cypress/e2e/basic.cy.js
@@ -1,16 +1,17 @@
-describe("sample test", () => {
-    beforeEach(() => {
-        cy.visit("/");
-    });
+// TODO: Will be used as template once starting doing e2e tests
+// describe("sample test", () => {
+//     beforeEach(() => {
+//         cy.visit("/");
+//     });
 
-    it("displays the resources text", () => {
-        cy.get("h1").contains("Hola Puto :)");
-    });
-    it("renders the Netlify logo image", () => {
-        cy.get("img")
-            .should("be.visible")
-            .and(($img) => {
-                expect($img[0].naturalWidth).to.be.greaterThan(0);
-            });
-    });
-});
+//     it("displays the resources text", () => {
+//         cy.get("h1").contains("Hola Puto :)");
+//     });
+//     it("renders the Netlify logo image", () => {
+//         cy.get("img")
+//             .should("be.visible")
+//             .and(($img) => {
+//                 expect($img[0].naturalWidth).to.be.greaterThan(0);
+//             });
+//     });
+// });
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,25 @@
 import Footer from "@components/Footer";
 import Header from "@components/Header";
+import { Player } from "@lottiefiles/react-lottie-player";
 import Head from "next/head";
 
 export default function Home() {
     return (
         <div className='container'>
             <Head>
-                <title>Next.js Starter!</title>
+                <title>Tomer Portfolio</title>
                 <link rel='icon' href='/favicon.ico' />
             </Head>
+            <Player
+                id='firstLottie'
+                autoplay
+                loop
+                mode='normal'
+                src='https://assets5.lottiefiles.com/packages/lf20_ylka2aru.json'
+                style={{ width: "300px", height: "300px" }}></Player>
 
             <main>
-                <Header title='Hola Puto :)' />
+                <Header title='Hola Hola :)' />
                 <p className='description'>
                     Empeza a hacerlo lindo ^_^
                     {/* Get started by editing <code>pages/index.js</code> */}
