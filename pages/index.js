39a90acd407170671ddcaa43f1470e892ce1f1c6
Nicolas Caro <trynx93@gmail.,com>
2022-07-05 01:01:04 +0300

    Add favicon images

--- pages/index.js
+++ pages/index.js
@@ -8,17 +8,30 @@ export default function Home() {
         <div className='container'>
             <Head>
                 <title>Tomer Portfolio</title>
-                <link rel='icon' href='/favicon.ico' />
+                <link rel='shortcut icon' href='/image/favicon.ico' />
+                <link
+                    rel='icon'
+                    type='image/png'
+                    sizes='32x32'
+                    href='/image/favicon-32x32.png'
+                />
+                <link
+                    rel='icon'
+                    type='image/png'
+                    sizes='16x16'
+                    href='/image/favicon-16x16.png'
+                />
+                <link rel='icon' href='/image/favicon.ico' />
             </Head>
-            <Player
-                id='firstLottie'
-                autoplay
-                loop
-                mode='normal'
-                src='https://assets5.lottiefiles.com/packages/lf20_ylka2aru.json'
-                style={{ width: "300px", height: "300px" }}></Player>
 
             <main>
+                <Player
+                    id='firstLottie'
+                    autoplay
+                    loop
+                    mode='normal'
+                    src='https://assets5.lottiefiles.com/packages/lf20_ylka2aru.json'
+                    style={{ width: "300px", height: "300px" }}></Player>
                 <Header title='Hola Hola :)' />
                 <p className='description'>
                     Empeza a hacerlo lindo ^_^
